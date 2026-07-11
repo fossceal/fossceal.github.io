acf75f7e998a6085491635f0f061eb721c3ee707
zyndor1548 <a.s.aromal2006@gmail.com>
2026-07-11 16:26:23 +0530

    final fix

--- team/teamMaker.js
+++ team/teamMaker.js
@@ -9,34 +9,34 @@ async function fetchandretjson() {
 		});
 }
 
-function generate(aqute, gif, name, role, pic, row, col, i, socials = {}) {
-    let socialLinks = "";
-    const icons = {
-        linkedin: "fa-brands fa-linkedin-in",
-        github: "fa-brands fa-github",
-        instagram: "fa-brands fa-instagram",
-        facebook: "fa-brands fa-facebook-f",
-        website: "fa-solid fa-globe" ,
+function generate(aqute, gif, name, role, pic, socials = {}) {
+	let socialLinks = "";
+	const icons = {
+		linkedin: "fa-brands fa-linkedin-in",
+		github: "fa-brands fa-github",
+		instagram: "fa-brands fa-instagram",
+		facebook: "fa-brands fa-facebook-f",
+		website: "fa-solid fa-globe",
 		email: "fa-solid fa-envelope",
 		hrb_customs: "fa-solid fa-cube"
-    };
+	};
 
-    for (let [platform, url] of Object.entries(socials)) {
-        if (icons[platform]) {
-            socialLinks += `
+	for (let [platform, url] of Object.entries(socials)) {
+		if (icons[platform]) {
+			socialLinks += `
                 <a href="${url}" target="_blank" class="social-icon ${platform}">
                     <i class="${icons[platform]}"></i>
                 </a>`;
-        }
-    }
+		}
+	}
 
-    return `
+	return `
         <div id="${aqute}" class="teamBOXXX">
             <div class="teamName">${name}</div>
             <div class="teamRole">${role}</div>
-            <img src="/teamPics/${pic}.webp" class="teamPic">
+            <img data-src="/teamPics/${pic}.webp" class="teamPic">
             <div class="shadow"></div>
-            <div class="gifcase" style="background: url('/teamGifs/${gif}');background-position: center;background-size: cover;"></div>
+			<div class="gifcase" data-gif="/teamGifs/${gif}" style="background-position: center;background-size: cover;"></div>
 
             <div class="socials">${socialLinks}</div>
         </div>
@@ -46,7 +46,6 @@ function generate(aqute, gif, name, role, pic, row, col, i, socials = {}) {
 
 async function main() {
 	let data = await fetchandretjson();
-// Added execom 2025 section
 
 	if (data["2026"]) {
 		let team2025 = elem("teamCont2026");
@@ -57,39 +56,64 @@ async function main() {
 				member.name,
 				member.role,
 				member.pic,
-				Math.floor(i / 5) + 1,
-				(i % 5) + 1,
-				i,
 				member.socials || {}
 			);
 		});
 	}
 
-	if (data["2025"]) {
-		let team = elem("teamCont");
-		data["2025"].forEach((member, i) => {
-			team.innerHTML += generate(
-				member.aqute,
-				member.gif,
-				member.name,
-				member.role,
-				member.pic,
-				Math.floor(i / 5) + 1,
-				(i % 5) + 1,
-				i
-			);
+	// if (data["2025"]) {
+	// 	let team = elem("teamCont2025");
+	// 	data["2025"].forEach((member, i) => {
+	// 		team.innerHTML += generate(
+	// 			member.aqute,
+	// 			member.gif,
+	// 			member.name,
+	// 			member.role,
+	// 			member.pic,
+	// 			member.socials || {}
+	// 		);
+	// 	});
+	// }
+	// if (data["2023"]) {
+	// 	let team = elem("teamCont2023");
+	// 	data["2023"].forEach((member, i) => {
+	// 		team.innerHTML += generate(
+	// 			member.aqute,
+	// 			member.gif,
+	// 			member.name,
+	// 			member.role,
+	// 			member.pic,
+	// 			member.socials || {}
+	// 		);
+	// 	});
+	// }
+	await load();
+}
+
+async function load() {
+
+	for (const i of document.querySelectorAll(".teamBOXXX")) {
+		const img = i.querySelector(".teamPic");
+		const gifDiv = i.querySelector(".gifcase");
+
+		await new Promise(resolve => {
+			img.onload = resolve;
+			img.onerror = resolve;
+			img.src = img.dataset.src;
+		});
+
+		await new Promise(resolve => {
+			const gifUrl = gifDiv.dataset.gif;
+			const loader = new Image();
+
+			loader.onload = () => {
+				gifDiv.style.backgroundImage = `url('${gifUrl}')`;
+				resolve();
+			};
+
+			loader.onerror = resolve;
+			loader.src = gifUrl;
 		});
 	}
 }
-
 main();
-
-function test() {
-	for (let i = 0; i < 20; i++) {
-		let col = (i % 5) + 1;
-		let row = Math.floor(i / 5) + 1;
-		console.log(row, col);
-	}
-}
-
-// added a if statement for execom separatin
